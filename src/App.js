31e06ddbeb7935d3428c8d9671499db8ddcf9b30
rahathossain3 <rahathossain210@gmail.com>
2022-04-19 03:26:48 +0600

    404 Notfound added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+
+
+        <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
 
