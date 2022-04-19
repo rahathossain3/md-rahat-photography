45c9665d7f9000273851a97e7637b28fa219079f
rahathossain3 <rahathossain210@gmail.com>
2022-04-19 16:36:30 +0600

    about my self added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h2>Thsi is about</h2>
+        <div className='container-fluid p5 py-5 bg-dark'>
+            <h1 className=' text-white '>About My self</h1>
+            <div className=' mt-5 outline'>
+                <img className='my-img'
+                    src="https://avatars.githubusercontent.com/u/96797007?v=4"
+                    alt="Md. Rahat Hossain Khan Imon"
+                    title='Md. Rahat Hossain Khan Imon' />
+
+                <div className=' about-text w-75 mx-auto'>
+                    <h3 className='my-4 mx-2'>
+                        <span className=' text-warning'>Name : </span>
+                        MD. Rahat Hossain Khan Imon
+                    </h3>
+
+                    <h5 className='my-4 mx-2'>
+                        <span className=' text-warning'>My Goals : </span>
+                        <br /><br />
+                        <span className='mt-3'>Want to make my self a good and respected Web developer and Designer also a Graphic Designer.
+                            Work for Helpless people make their happiness.  To build a pollution less better world for human.  </span>
+                    </h5>
+
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
 
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
                         <Nav.Link eventKey={2} href="#memes">
                             Dank memes
                         </Nav.Link>
